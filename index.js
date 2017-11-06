289aac070220f3d348c47749e10a7e44aaee0f98
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 21:26:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays()
 {return "Happy holidays!"}
-var name = "you"
+
 function happyHolidaysTo(name)
-{return "Happy holidays, ${name}!"}
+{return `Happy holidays, ${name}!`}
